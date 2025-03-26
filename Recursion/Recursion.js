479020b77bc2ac3cd17583d047ae6f65694d5466
fFukurou <supevi1@gmail.com>
2025-03-26 20:37:38 -0300

    recursion start

---
+++ Recursion/Recursion.js
@@ -0,0 +1,16 @@
+function funcThree() {
+    console.log('Three');
+}
+
+function funcTwo() {
+    funcThree();
+    console.log('Two');
+}
+
+function funcOne() {
+    funcTwo();
+    console.log('One');
+}
+
+
+funcOne();
